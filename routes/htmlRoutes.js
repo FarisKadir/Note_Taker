2a0d6da25a71db1736c6866f134625e1011cbb90
Faris Kadir <faris.kadir@gmail.com>
2021-03-06 17:31:05 -0500

    Updating html route.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,11 +12,11 @@ module.exports = (app) => {
 
     // Display notes.html when /notes is accessed
     app.get('/notes', function(req,res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
     });
     
     // Display index.html when all other routes are accessed
     app.get('/', function(req,res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "./public/index.html"));
     });
 };
